272ca09d4c785f763f4c5ac93e7e7e52d8bc4aef
Tri Nguyen <tringuye@mines.edu>
2012-11-01 12:08:58 -0600

    Fix draw Board.

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,20 +1,37 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.MenuBar;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 
 public class ClueGame extends JFrame {
 
 	public ClueGame() {
+		super();
+		// Adding Board to JFrame
 		Board gameBoard = new Board();
+		try {
+			gameBoard.loadConfigFiles("ClueBoardLegend.txt", "ClueBoardLayout.csv", "weapons.txt", "players.txt");
+		} catch (BadConfigFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		setLayout(new BorderLayout());
-		setSize(gameBoard.getNumColumns()*gameBoard.CELLSIZE, gameBoard.getNumRows()*gameBoard.CELLSIZE);
+		setSize(gameBoard.getNumColumns()*gameBoard.CELLSIZE+17, gameBoard.getNumRows()*gameBoard.CELLSIZE+17);
 		add(gameBoard, BorderLayout.CENTER);
+		
+		// Adding the file menu to JFrame
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 	}
 	
 	public static void main(String[] args) {
-		JFrame game = new JFrame();
+		ClueGame game = new ClueGame();
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.setVisible(true);
 	}
