29be93e9a84d9c50f72c96088fccdc3573c256ad
N Harvey <nthnhrvy@gmail.com>
2012-11-01 12:38:21 -0600

    not sure what this is

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class ClueGame extends JFrame {
 
